dbac9ac5bef318ce466df63ec6102cce3acec96e
Richard Heard <richie@rheard.com>
2013-04-12 01:07:45 -0700

    Add a dedicated FMDB.h header that includes all *.h. Clean up the libraries header paths so that parent projects can find headers more easily. ie: #import <FMDB/FMDB.h>

---
+++ FMDB.h
@@ -0,0 +1,13 @@
+//
+//  FMDB.h
+//  fmdb
+//
+//  Created by Richard Heard on 11/04/13.
+//  Copyright 2013 Richard Heard. All rights reserved.
+//
+
+#import "FMDatabase.h"
+#import "FMDatabaseQueue.h"
+#import "FMDatabaseAdditions.h"
+#import "FMDatabasePool.h"
+#import "FMResultSet.h"
