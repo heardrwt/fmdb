ca0f91b7722ad85727a142fe638801073817da7b
Richard Heard <richie@rheard.com>
2013-07-30 00:58:11 -0700

    Move FMDB.h into the src folder.

--- src/FMDB.h
+++ src/FMDB.h
@@ -2,9 +2,6 @@
 //  FMDB.h
 //  fmdb
 //
-//  Created by Richard Heard on 11/04/13.
-//  Copyright 2013 Richard Heard. All rights reserved.
-//
 
 #import "FMDatabase.h"
 #import "FMDatabaseQueue.h"
